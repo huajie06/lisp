99896741a978d3429828aae546f4f760ea43f3ca
Huajie Zhang (Mike) <huajie06@gmail.com>
2022-07-11 20:43:13 -0400

    some update on the map functions

--- clisp/pcl-ch10-funcs-on-list.lisp
+++ clisp/pcl-ch10-funcs-on-list.lisp
@@ -1,3 +1,128 @@
+;; funcall vs. apply
+(funcall #'(lambda (x y)(+ x y)) 1 2)
+(apply #'(lambda (x y)(+ x y)) '(1 2))
+
+;; apply takes a list, funcall not a list
+(apply #'+ '(1 2 3))
+(apply #'+ 12 3 nil)
+(apply #'(lambda (x y) (* x y)) '(1 2))
+
+(lambda (x y) (* x y))(1 2) ; this will not eval!!
+
+;;===================================================================
+;; ========= mapcar, mapc and mapcan on element of list =============
+;;========== maplist, mapl, mapcon on sublist =======================
+;;===================================================================
+
+
+;; mapcar, the FN must take as many parameters as there are lists
+(mapcar #'+ '(1 2) '(2 3 4)) ; + takes as many
+(mapcar #'car '((1 a) (2 b) (3 c))) ; car takes 1
+(mapcar #'(lambda (x) (+ 1 x)) '(1 2 3)) ; takes 1
+(mapcar #'(lambda (x y) (+ x y)) '(1 2) '(6 3)) ; takes 2
+(mapcar #'+ '(1 2 3) '(2 3 4))
+
+(mapcar #'+ '('(1 2 3) '(2 3 4))) ; no work
+
+;; mapc on process the input for side effect, and return the first input
+(mapc #'(lambda (x y) (print (* x y))) (list 1 0 2) (list 3 4 5))
+(mapc #'+ '(1 2 3))
+(mapc #'+ '(1 2 3) '(2 3 4))
+
+(mapcar #'list (list 1 2 3) (list 4 5 6))
+
+;; compbine apply and mapcar, you can do this
+(mapcar
+ #'(lambda (x) (apply #'+ x))
+ '((1 2 3) (3 4 5)))
+
+(mapcar
+ #'(lambda (x) (apply #'(lambda (a b c) (* a b c)) x))
+ '((1 2 3) (3 4 5)))
+(mapcar
+ #'(lambda (x) (apply #'* x))
+ '((1 2 3) (3 4 5)))
+
+
+;;map
+(map 'list #'(lambda (x y) (mod (+ x y) 16))
+     '(1 2 3 4)
+     '(10 9 8 7))
+
+(map 'vector #'(lambda (x y) (mod (+ x y) 16))
+     '(1 2 3 4)
+     '(10 9 8 7))
+
+(map 'list #'(lambda (x y) (or (equal y 10) y))
+     '(1 2 3 4)
+     '(10 9 8 7))
+
+(map 'vector #'(lambda (x y) (and (equal y 10) x))
+     '(1 2 3 4)
+     '(10 9 8 7))
+
+
+;; some interesting cases?
+(mapcar '#+ '((1 2 3) (3 4 5 6))) ;; this doesn't work
+;;=========================================
+(apply #'mapcar #'+ '((1 2 3) (3 4 5 6)))
+(mapcar #'(lambda (x) (apply #'+ x)) '((1 2 3) (2 3 4)))
+;;=========================================
+
+(funcall #'mapcar #'+ '(1 2 3) '(1 2))
+(mapcar #'+ '(1 2 3) '(1 2))
+
+(apply #'mapcar #'(lambda (&rest rest)(print rest)) '((1 2 3) (3 4 5 6)))
+(apply #'(lambda (x y) (print (list x y))) '('a 'b))
+
+;;;
+(mapcar #'(lambda (rest) (apply #'+ rest)) '((1 2 3) (2 3 4)))
+(mapcar #'(lambda (a b c) (+ a b c)) '(1 2 3) '(2 3 4) '(1 0 0))
+(mapcar #'(lambda (&rest rest) (apply #'+ rest)) '(1 2 3))
+(mapcar #'(lambda (&rest rest) (print rest)) '(1 2 3))
+
+(apply #'mapcar #'list '((1 2 3) (3 4 5 6)))
+
+;; transpose
+(defparameter *lists* '((a0 a1 a2 a3) (b0 b1 b2 b3) (c0 c1 c2 c3)))
+(apply #'mapcar #'list *lists*)
+(funcall #'mapcar #'list '(a0 a1 a2 a3) '(b0 b1 b2 b3) '(c0 c1 c2 c3))
+(mapcar #'list '(a0 a1 a2 a3) '(b0 b1 b2 b3) '(c0 c1 c2 c3))
+
+(apply #'(lambda (&rest rest) (apply #'+ rest)) '(1 2 3 4))
+
+;; mapcan
+(mapcan #'(lambda (x) (+ 1 x)) '(1 2 9))
+(mapcan #'list (list 1 2 3) (list 4 5 6))
+
+
+;; maplist
+(maplist #'(lambda (x) (cons 'foo x))
+         '(a b c d))
+
+;; multiple collect
+(loop for i from 0 to 12 by 3
+      collect i)
+
+(loop for i from 1 to 3
+      collect i collect (* 2 i))
+
+(loop for i from 1 to 3
+      collect i collect (* 2 i) collect i)
+==
+
+
+
+
+
+
+
+
+
+
+
+
+
 (let ((a 1))
   (print a)
   (print (+ a 1))
@@ -13,6 +138,10 @@
      (j 0 (+ 2 j)))
     ((> (* i j) 50))
   (format t "i = ~a, j = ~a~%"i j))
+
+
+(nconc '(12 2) '(2 3))
+(append '(1 2) '(3 4))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;                                                      ;;;
@@ -145,4 +274,3 @@ tb
 
 
 (maphash #'(lambda (k v) (format t "key: ~a, value: ~a~%" k v)) tb)
-
